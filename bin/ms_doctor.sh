03b2eb57cd64b36cb05b7e0ddf3ee0bc514d1d66
Nretnil Kram <signkram@gmail.com>
2016-04-14 11:20:52 -0400

    directory permission check in ms doctor

--- bin/ms_doctor.sh
+++ bin/ms_doctor.sh
@@ -6,10 +6,42 @@
 
 found_issue=false
 
+#   Check permissions on /usr/local/bin
+#   ---------------------------------------
+if [[ ! "$(whoami)" == "$(stat /usr/local/bin/ | awk '{print $5}')" ]]
+	then
+	echo "
+/user/local/bin does not have $(whoami) as the owner which could cause issues with brew installs and updates.
+
+run:
+
+sudo chown -R $(whoami) /usr/local/bin
+
+to resolve this issue.
+	"
+found_issue=true
+fi
+
+#   Check permissions on /usr/local/lib
+#   ---------------------------------------
+if [[ ! "$(whoami)" == "$(stat /usr/local/lib/ | awk '{print $5}')" ]]
+	then
+	echo "
+/user/local/bin does not have $(whoami) as the owner which could cause issues with brew installs and updates.
+
+run:
+
+sudo chown -R $(whoami) /usr/local/lib
+
+to fix this.
+	"
+found_issue=true
+fi
+
 #   Check that bash_config is being sourced
 #   ---------------------------------------
 if !( grep -Fq "source ~/.mac_salt/bash_config" ~/.bash_profile ~/.bashrc &> /dev/null )
-then
+	then
 	echo -e "
 Add the following to .bash_profile or .bashrc to source the MAC SALT bash configuration: 
 
@@ -23,7 +55,7 @@ fi
 #   Check that ssh/config is setup for corkscrew
 #   ---------------------------------------
 if !( grep -Fq "Host *" ~/.ssh/config )
-then
+	then
 	echo -e "
 You will want to setup a 'Host *' entry to your .ssh/config file to use corcskrew. 
 Seperate your ssh Proxy settings with a comment at the bottom of your ssh config file.
@@ -37,7 +69,7 @@ found_issue=true
 fi
 
 if !( $found_issue )
-then
+	then
 	echo -e "
 Ready to Go!
 	"
