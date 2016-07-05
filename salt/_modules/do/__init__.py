3a1e3ee64cc01a3992054ef26557796ae53df312
Nretnil Kram <signkram@gmail.com>
2016-07-05 15:34:28 -0400

    added output level --verbose

--- salt/_modules/do/__init__.py
+++ salt/_modules/do/__init__.py
@@ -11,9 +11,10 @@ def test():
 def update():
   ''' Update local Salt Config '''
   log.info("Updating Salt Config")
-  log.info( __salt__['git.pull'](__grains__['mac_salt_home']) )
+  log.info( __salt__['git.pull'](__grains__['mac_salt_home'],user=__grains__['user']) )
   log.info( "Salt Config Updated" )
 
 __outputter__ = {
+  'test': 'txt',
   'update': 'txt'
   }
--- salt/_modules/go/__init__.py
+++ salt/_modules/go/__init__.py
@@ -3,10 +3,16 @@ import os
 
 log = logging.getLogger(__name__)
 
+def test():
+  ''' Module Exitence Test '''
+  log.info( 'go.test exists' )
+  return True
+
 def big():
   ''' Running High State '''
   __salt__['state.highstate'](test=False)
 
 __outputter__ = {
+  'test': 'txt',
   'update': 'txt'
   }
